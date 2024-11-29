a609e11c9fc62d108874ec97cfdb9828a1108d38
Simon Fredriksson <simon.fredriksson93@gmail.com>
2024-11-29 16:04:49 +0100

    Add utility functions for date and hour generation, room booking checks, and date formatting

---
+++ app/utils/helperMethods.tsx
@@ -0,0 +1,53 @@
+// Generates an array of date strings for a given number of days starting
+
+import { Booking, Room } from "../services/apiClient";
+
+// from a specified date. Default start date is today.
+export const generateDates = (
+  numDays: number,
+  startDate: Date = new Date()
+): string[] => {
+  const dates: string[] = [];
+  const start = new Date(startDate);
+  for (let i = 0; i < numDays; i++) {
+    const date = new Date(start);
+    date.setDate(start.getDate() + i);
+    dates.push(date.toISOString().split("T")[0]); // Format date as 'YYYY-MM-DD'
+  }
+  return dates;
+};
+
+// Generates an array of hour strings between a specified start and end hour
+export const generateHours = (startHour: number, endHour: number): string[] => {
+  const hours: string[] = [];
+  for (let i = startHour; i <= endHour; i++) {
+    // Add leading zero to single digit hours
+    hours.push(`${i < 10 ? "0" : ""}${i}:00`); // Format hour as 'HH:00'
+  }
+  return hours;
+};
+
+// Checks if a room is booked at a specific date and hour
+export const isRoomBooked = (room: Room, date: Date, hour: string): boolean => {
+  const startTime = new Date(`${date}T${hour}:00.000Z`); // Create start time from date and hour
+  const endTime = new Date(startTime);
+  endTime.setHours(endTime.getHours() + 1); // Set end time to one hour after start time
+
+  // Check if any booking in the room matches the start time
+  return room.bookings.some((booking: Booking) => {
+    const bookingStart = new Date(booking.startTime);
+    const bookingEnd = new Date(booking.endTime);
+    return (
+      // Check if booking start and end times match.
+      bookingStart.getTime() === startTime.getTime() &&
+      bookingEnd.getTime() === endTime.getTime()
+    );
+  });
+};
+
+// Format date to 'day month' format in Swedish without trailing period
+export const formatDate = (date: Date): string => {
+  return new Date(date)
+    .toLocaleDateString("sv-SE", { day: "numeric", month: "short" })
+    .replace(".", "");
+};
